edc6b276305add5961b8b55a4ea8407eb35fadf4
Raga Ananth <raga@Ragas-MacBook-Pro.local>
2022-02-21 16:37:24 +0000

    add files

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# company
+variable "company" {
+  type        = string
+  description = "This variable defines the name of the company"
+}
+# environment
+variable "environment" {
+  type        = string
+  description = "This variable defines the environment to be built"
+}
+# azure region
+variable "location" {
+  type        = string
+  description = "Azure region where the resource group will be created"
+}
